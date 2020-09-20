64a4cb3d7ade25234ea0cc27be444af4c7c0286d
westodra-prog <71451457+westodra-prog@users.noreply.github.com>
2020-09-20 13:38:09 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,6 +2,20 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`0200020001010101`, img`
+1 . 
+1 . 
+`, [myTiles.transparency16,sprites.castle.tileGrass1], TileScale.Sixteen)
+        }
+        return null;
+    })
 
 }
 // Auto-generated code. Do not edit.
